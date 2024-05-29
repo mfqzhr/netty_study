a704593d8ccacbb10c23d1facd515a2031c152b4
mufanqiang <mufanqiang@jd.com>
2024-05-29 16:59:31 +0800

    feat(netty api byteBuf):

--- netty-nio/src/main/java/com/mfq/nio/netty/client/NettyClient.java
+++ netty-nio/src/main/java/com/mfq/nio/netty/client/NettyClient.java
@@ -1,12 +1,16 @@
 package com.mfq.nio.netty.client;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 /**
@@ -16,12 +20,28 @@ public class NettyClient {
     public static void main(String[] args) throws InterruptedException {
         Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
-        bootstrap.group(group)
+        bootstrap
+                //指定线程模型
+                .group(group)
+                //指定io为NIO
                 .channel(NioSocketChannel.class)
+                //IO处理逻辑
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel channel) throws Exception {
-                        channel.pipeline().addLast(new StringEncoder());
+                        channel.pipeline()
+                                .addLast(new StringEncoder())
+                                //ChannelInboundHandlerAdapter 会在建立连接后调用
+                                .addLast(new ChannelInboundHandlerAdapter(){
+                                    @Override
+                                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                        System.out.println(new Date() + " : " + "客户端写出数据");
+                                        // 1. 获取数据
+                                        ByteBuf buffer = ctx.alloc().buffer();
+                                        buffer.writeBytes("hello i am a client".getBytes(StandardCharsets.UTF_8));
+                                        ctx.channel().writeAndFlush(buffer);
+                                    }
+                                });
                     }
                 });
         Channel channel = bootstrap.connect("127.0.0.1", 38888).channel();
--- netty-nio/src/main/java/com/mfq/nio/netty/server/NettyServer.java
+++ netty-nio/src/main/java/com/mfq/nio/netty/server/NettyServer.java
@@ -1,13 +1,19 @@
 package com.mfq.nio.netty.server;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
 
 /**
  * nio-server-netty
@@ -18,17 +24,32 @@ public class NettyServer {
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup worker = new NioEventLoopGroup();
         serverBootstrap
+                //指定线程模型
                 .group(boss, worker)
+                //指定IO类型为NIO
                 .channel(NioServerSocketChannel.class)
+                //IO处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ch.pipeline()
-                                .addLast(new StringDecoder())
-                                .addLast(new SimpleChannelInboundHandler<String>() {
+ //                               .addLast(new StringDecoder())
+//                                .addLast(new SimpleChannelInboundHandler<String>() {
+//                                    @Override
+//                                    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+//                                        System.out.println(s);
+//                                    }
+//                                })
+                                .addLast(new ChannelInboundHandlerAdapter() {
                                     @Override
-                                    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
-                                        System.out.println(s);
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        ByteBuf byteBuf = (ByteBuf) msg;
+                                        System.out.println("maxCapacity : " + byteBuf.maxCapacity());
+                                        // byteBuf所占用的
+                                        System.out.println("capacity : " + byteBuf.capacity());
+                                        System.out.println("readerIndex : " + byteBuf.readerIndex());
+                                        System.out.println("writerIndex : " + byteBuf.writerIndex());
+                                        System.out.println("hello i am server, you are message is " + byteBuf.toString(StandardCharsets.UTF_8));
                                     }
                                 });
                     }
